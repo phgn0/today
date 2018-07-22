d60afec6af360401a0433109b37b15f4458577ec
Peter Hagen <phgn@protonmail.ch>
2018-07-22 17:16:52 +0200

    Add basic react, wepack setup

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist")
+    },
+    plugins: [new CopyWebpackPlugin([{ from: "public/", to: "." }])]
+};
